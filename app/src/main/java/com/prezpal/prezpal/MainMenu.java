066f0b3d1e2abe84ab36d172b5fb9fd66ada237a
Brian McCarthy <brianmcca1@gmail.com>
2018-02-20 18:52:13 -0500

    Integrating David's interfaces with main project

---
+++ app/src/main/java/com/prezpal/prezpal/MainMenu.java
@@ -0,0 +1,32 @@
+package com.prezpal.prezpal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void launchPresentationMode(View view) {
+    }
+    public void launchPresentationSettings(View view) {
+        Intent intent = new Intent(this, PresentationSettings.class);
+        startActivity(intent);
+    }
+    public void launchInterviewMode(View view) {
+    }
+    public void launchInterviewSettings(View view){
+        Intent intent = new Intent(this, InterviewSettings.class);
+        startActivity(intent);
+    }
+    public void launchHelpInstructions(View view){
+        Intent intent = new Intent(this, HelpInstructions.class);
+        startActivity(intent);
+    }
+}
