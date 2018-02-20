725d2c27828f2ee36b0c9b83d525762a70f060bb
DTang <davidtang997@gmail.com>
2018-02-20 18:19:31 -0500

    Added an otherwise empty project with the UI.
    Buttons link between the first few pages. Need to implement the video and results stuff, by transferring this stuff there or that stuff here

---
+++ PrezPal UI Setup/app/src/main/java/com/example/david/prezpal/PresentationSettings.java
@@ -0,0 +1,13 @@
+package com.example.david.prezpal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PresentationSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.presentation_settings);
+    }
+}
